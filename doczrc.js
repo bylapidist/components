1a7f52803acb60d7a34018de80f7327141355289
Brett Dorrans <hello@lapidist.net>
2019-11-29 18:59:14 +0000

    initial commit

---
+++ doczrc.js
@@ -0,0 +1,10 @@
+export default {
+    themeConfig: {
+        showLiveError: false
+    },
+    typescript: true,
+    dest: './docs',
+    codeSandbox: false,
+    port: 4000,
+    files: 'packages/**/*.mdx'
+};
