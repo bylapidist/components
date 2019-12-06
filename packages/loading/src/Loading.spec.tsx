02faccbeab9d167589fa38d0abc73a18649d9f5b
Brett Dorrans <hello@lapidist.net>
2019-12-06 13:31:06 +0000

    feat(@lapidist/loading): init (#9)
    * feat(@lapidist/loading): init
    
    * feat(@lapidist/loading): initial implementation
    
    * style: prettier

---
+++ packages/loading/src/Loading.spec.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import Loading from './index';
+
+test('it works', () => {
+    const tree = renderer.create(<Loading />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
---
+++ packages/loading/src/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import { absolute, fillParent } from '@lapidist/design-tokens';
+import { defaultTheme } from '@lapidist/theme-provider';
+import Logo, { LogoProps } from '@lapidist/logo';
+
+export interface LoadingProps {
+    /** The Loading's id. */
+    readonly id?: string;
+    /** The Loading's classname. */
+    readonly className?: string;
+    /** The Loading's size. */
+    readonly size?: string;
+    /** The Loading's color. */
+    readonly color?: string;
+}
+
+const LoadingLogo: React.FC<LogoProps> = styled(Logo)`
+    ${absolute()};
+    ${fillParent()};
+
+    margin: auto;
+    opacity: 0.9;
+    width: ${(props): string => props.size};
+`;
+
+const LoadingContainer: React.FC<LoadingProps> = ({
+    id,
+    className,
+    size = defaultTheme.sizing.xl,
+    color
+}) => (
+    <div id={id} className={className}>
+        <LoadingLogo
+            bUpColor={color}
+            bDownColor={color}
+            dColor={color}
+            size={size}
+            animated={false}
+            thinking={true}
+        />
+    </div>
+);
+
+const Loading: React.FC<LoadingProps> = styled(LoadingContainer)<LoadingProps>`
+    ${absolute()};
+    ${fillParent()};
+`;
+
+export default Loading;
