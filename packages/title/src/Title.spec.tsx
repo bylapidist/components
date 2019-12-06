0c3a25dc405094e859a9b62ceaba1eb5a297146e
Brett Dorrans <hello@lapidist.net>
2019-12-06 02:59:36 +0000

    feat(@lapidist/title): initial implementation

--- packages/title/src/Title.spec.tsx
+++ packages/title/src/Title.spec.tsx
@@ -4,13 +4,32 @@ import 'jest-styled-components';
 
 import Title from './index';
 
-test('it works', () => {
-    const tree = renderer
-        .create(
-            <Title>
-                <p>Hello world!</p>
-            </Title>
-        )
-        .toJSON();
+test('it works as h1', () => {
+    const tree = renderer.create(<Title size={1}>H1</Title>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('it works as h2', () => {
+    const tree = renderer.create(<Title size={2}>H2</Title>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('it works as h3', () => {
+    const tree = renderer.create(<Title size={3}>H3</Title>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('it works as h4', () => {
+    const tree = renderer.create(<Title size={4}>H4</Title>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('it works as h5', () => {
+    const tree = renderer.create(<Title size={5}>H5</Title>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('it works as h6', () => {
+    const tree = renderer.create(<Title size={6}>H6</Title>).toJSON();
     expect(tree).toMatchSnapshot();
 });
