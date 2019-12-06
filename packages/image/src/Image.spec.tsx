19f4cae830fdfa3bbbdb799c1851977ca6bc8d80
Brett Dorrans <hello@lapidist.net>
2019-12-06 14:03:53 +0000

    feat(@lapidist/image): init (#14)
    * feat(@lapidist/image): init
    
    * feat(@lapidist/image): initial implementation

---
+++ packages/image/src/Image.spec.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import Image from './index';
+
+test('it works', () => {
+    const tree = renderer
+        .create(<Image src="https://placehold.it/500x500" alt="image" />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
