24b8f9cadbcfaa2d6d4cffddb146798062143782
Brett Dorrans <hello@lapidist.net>
2019-12-06 01:30:04 +0000

    feat(@lapidist/paragraph): init
    .
    
    .
    
    .
    
    .
    
    .

---
+++ packages/paragraph/src/Paragraph.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+import Paragraph from './index';
+
+test('it works', () => {
+    const tree = renderer.create(<Paragraph>Hello world!</Paragraph>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('it works with size', () => {
+    const tree = renderer
+        .create(<Paragraph size="large">Hello world!</Paragraph>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('it works with weight', () => {
+    const tree = renderer
+        .create(<Paragraph weight="bold">Hello world!</Paragraph>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
