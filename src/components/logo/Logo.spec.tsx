cbc609bf117718bdbf94df164c03d10a659dd4d7
Brett Dorrans <hello@lapidist.net>
2020-05-24 00:37:36 +0100

    refactor(logo): fix #146 (#147)

--- src/components/logo/Logo.spec.tsx
+++ src/components/logo/Logo.spec.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import 'jest-styled-components';
 
-import { Logo } from './index';
+import { Logo, logoAnimation } from './index';
+import { slideUpLeft, thinking } from '../design-tokens';
+import { defaultTheme } from '../theme-provider';
 
 test('it works', () => {
     const tree = renderer.create(<Logo size="32px" />).toJSON();
@@ -26,4 +28,54 @@ test('it works with different colours', () => {
         )
         .toJSON();
     expect(tree).toMatchSnapshot();
+});
+
+test('logoAnimation applies thinking animation', () => {
+    const animation = logoAnimation(
+        {
+            thinking: true,
+            points: '0,0 0,16 16,16',
+            animated: false,
+            color: defaultTheme.colors.greens.base
+        },
+        null,
+        thinking()
+    );
+
+    const css = JSON.parse(JSON.stringify(animation[4]));
+
+    expect(css).toEqual({
+        name: 'hzFDkX',
+        id: 'sc-keyframes-hzFDkX',
+        stringifyArgs: [
+            '\n    0% { opacity: 1 }\n    50% { opacity: 0.6 }\n    100% { opacity: 1 }\n',
+            'hzFDkX',
+            '@keyframes'
+        ]
+    });
+});
+
+test('logoAnimation applies sliding animation', () => {
+    const animation = logoAnimation(
+        {
+            thinking: false,
+            points: '0,0 0,16 16,16',
+            animated: true,
+            color: defaultTheme.colors.greens.base
+        },
+        slideUpLeft(),
+        null
+    );
+
+    const css = JSON.parse(JSON.stringify(animation[3]));
+
+    expect(css).toEqual({
+        name: 'fOlPOW',
+        id: 'sc-keyframes-fOlPOW',
+        stringifyArgs: [
+            '\n    0% { transform: translate3d(100%,100%,0) }\n    80% { transform: translateZ(0) }\n    90% { transform: translate3d(5%,5%,0) }\n    to { transform: translateZ(0) }\n',
+            'fOlPOW',
+            '@keyframes'
+        ]
+    });
 });
