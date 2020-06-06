fd8a2a6698ac8bd3af7555e2fe52c2df1baf7003
Brett Dorrans <hello@lapidist.net>
2020-06-06 21:43:11 +0100

    fix(heading): duplicate code

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, {
+    AnyStyledComponent,
+    StyledComponent,
+    ThemedStyledFunction
+} from 'styled-components';
 import { PropsWithIdAndClassname } from '../../utilities';
-import { getFontSize, getFontWeight } from '../theme-provider/theme';
+import {
+    getFontSize,
+    getFontWeight,
+    NumberOrString
+} from '../theme-provider/theme';
 import { Text, TextProps } from '../text';
 
 export type HeadingSize = 1 | 2 | 3 | 4 | 5 | 6;
@@ -11,40 +19,25 @@ export interface HeadingProps extends TextProps, PropsWithIdAndClassname {
 }
 
 interface HeadingMapper {
-    [level: number]: React.FC<HeadingProps>;
+    [level: number]: StyledComponent<AnyStyledComponent, {}, HeadingProps>;
 }
 
-const H1: React.FC<HeadingProps> = styled.h1`
-    ${(props): string => `font-size: ${getFontSize(props.theme, 9)}`};
-`;
-
-const H2: React.FC<HeadingProps> = styled.h2`
-    ${(props): string => `font-size: ${getFontSize(props.theme, 8)}`};
-`;
-
-const H3: React.FC<HeadingProps> = styled.h3`
-    ${(props): string => `font-size: ${getFontSize(props.theme, 7)}`};
-`;
-
-const H4: React.FC<HeadingProps> = styled.h4`
-    ${(props): string => `font-size: ${getFontSize(props.theme, 6)}`};
-`;
-
-const H5: React.FC<HeadingProps> = styled.h5`
-    ${(props): string => `font-size: ${getFontSize(props.theme, 5)}`};
-`;
-
-const H6: React.FC<HeadingProps> = styled.h6`
-    ${(props): string => `font-size: ${getFontSize(props.theme, 4)}`};
+export const createHeading = (
+    heading: ThemedStyledFunction<AnyStyledComponent, {}>,
+    size: NumberOrString
+): StyledComponent<AnyStyledComponent, {}, HeadingProps> => heading<
+    HeadingProps
+>`
+    ${(props): string => `font-size: ${getFontSize(props.theme, size)}`};
 `;
 
 const headings: HeadingMapper = {
-    1: H1,
-    2: H2,
-    3: H3,
-    4: H4,
-    5: H5,
-    6: H6
+    1: createHeading(styled.h1, 9),
+    2: createHeading(styled.h2, 8),
+    3: createHeading(styled.h3, 7),
+    4: createHeading(styled.h4, 6),
+    5: createHeading(styled.h5, 5),
+    6: createHeading(styled.h6, 4)
 };
 
 const HeadingBySize: React.FC<HeadingProps> = ({
