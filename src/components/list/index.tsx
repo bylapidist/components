7861b470ebca43e4d09dad44c81008ec4fbd379a
Brett Dorrans <hello@lapidist.net>
2022-04-24 20:01:28 +0100

    Update index.tsx

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -42,7 +42,7 @@ const Items: React.FC<{ items: ListItemProps[] }> = ({ items }) => (
 );
 
 const OrderedOrUnordered = (
-    Component: React.FC<{ children: Element; }>,
+    Component: React.FC<{ children: Element }>,
     items: ListItemProps[]
 ): JSX.Element => (
     <Component>
