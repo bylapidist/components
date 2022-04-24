80962e5dfd75848511f1b9e9ab4a5f733b80d787
Brett Dorrans <hello@lapidist.net>
2022-04-24 20:06:22 +0100

    fix: List prop type (#1064)
    * fix: List prop type
    
    * Update index.tsx

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -42,7 +42,7 @@ const Items: React.FC<{ items: ListItemProps[] }> = ({ items }) => (
 );
 
 const OrderedOrUnordered = (
-    Component: React.FC,
+    Component: React.FC<BoxProps>,
     items: ListItemProps[]
 ): JSX.Element => (
     <Component>
