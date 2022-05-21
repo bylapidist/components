4b9d99885d9195864f69c462701c24d821281850
Brett Dorrans <hello@lapidist.net>
2022-05-21 19:36:47 +0100

    feat: add href to PanelHeading (#1090)

--- src/components/panel-heading/index.tsx
+++ src/components/panel-heading/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { withTheme } from 'styled-components';
 import { mergeStyles, Theme } from '@lapidist/styles';
 import { BoxProps } from '../box';
-import { panelHeadingStyles } from './styles';
+import { panelHeadingStyles, innerLinkStyles } from './styles';
 import { Heading, HeadingSize } from '../heading';
+import { Link } from '../link';
 
 export * from './styles';
 
@@ -13,6 +14,9 @@ export interface PanelHeadingProps {
     readonly kind: string;
     readonly theme: Theme;
     readonly size?: HeadingSize;
+    readonly href?: string;
+    readonly target?: string;
+    readonly rel?: string;
 }
 
 const BasePanelHeading: React.FC<PanelHeadingPropType & PanelHeadingProps> = ({
@@ -22,6 +26,9 @@ const BasePanelHeading: React.FC<PanelHeadingPropType & PanelHeadingProps> = ({
     theme,
     children,
     size,
+    href,
+    target,
+    rel,
     ...restProps
 }) => (
     <Heading
@@ -30,7 +37,18 @@ const BasePanelHeading: React.FC<PanelHeadingPropType & PanelHeadingProps> = ({
         styles={mergeStyles(panelHeadingStyles({ kind, theme }), styles)}
         {...restProps}
     >
-        {children}
+        {href ? (
+            <Link
+                styles={innerLinkStyles({ kind, theme })}
+                target={target}
+                rel={rel}
+                href={href}
+            >
+                {children}
+            </Link>
+        ) : (
+            children
+        )}
     </Heading>
 );
 
