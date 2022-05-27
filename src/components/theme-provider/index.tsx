3abfcd1c5a289136bb4310282f0038fae7511fdc
Brett Dorrans <hello@lapidist.net>
2022-05-27 18:29:05 +0100

    Update index.tsx

--- src/components/theme-provider/index.tsx
+++ src/components/theme-provider/index.tsx
@@ -69,7 +69,8 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({
 
     React.useEffect(() => {
         const prefersDarkTheme =
-            (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ||
+            (window.matchMedia &&
+                            window.matchMedia('(prefers-color-scheme: dark)').matches) ||
             localStorage.getItem('isDarkMode') === 'true';
         setThemeContext(
             prefersDarkTheme ? mergeThemes(baseTheme, darkTheme) : baseTheme
