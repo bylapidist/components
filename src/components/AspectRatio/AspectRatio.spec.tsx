cb874c5f21ef1e1940d37aea388ab96a1efb9a94
Brett Dorrans <hello@lapidist.net>
2022-10-14 23:23:43 +0100

    feat: wrap in describe

--- src/components/AspectRatio/AspectRatio.spec.tsx
+++ src/components/AspectRatio/AspectRatio.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { expect, test, afterEach } from 'vitest';
+import { describe, expect, test, afterEach } from 'vitest';
 import { cleanup, render, screen } from '@testing-library/react';
 
 import AspectRatio, { Ratio } from './index';
@@ -10,43 +10,45 @@ afterEach(cleanup);
 
 const ratios: Ratio[] = ['1/1', '1/2', '3/2', '4/3', '16/9', '9/16'];
 
-test.each(ratios)('it works with ratios', (ratio) => {
-    const { container } = setup(
-        <AspectRatio ratio={ratio}>Hello world</AspectRatio>
-    );
-    expect(container.firstChild).toMatchSnapshot();
-});
+describe('AspectRatio', () => {
+    test.each(ratios)('it works with ratios', (ratio) => {
+        const { container } = setup(
+            <AspectRatio ratio={ratio}>Hello world</AspectRatio>
+        );
+        expect(container.firstChild).toMatchSnapshot();
+    });
 
-test('it works with default testId', () => {
-    setup(<AspectRatio ratio={ratios[0]}>Hello world</AspectRatio>);
-    expect(screen.getByTestId('AspectRatio')).toBeTruthy();
-});
+    test('it works with default testId', () => {
+        setup(<AspectRatio ratio={ratios[0]}>Hello world</AspectRatio>);
+        expect(screen.getByTestId('AspectRatio')).toBeTruthy();
+    });
 
-test('it works with specified testId', () => {
-    setup(
-        <AspectRatio ratio={ratios[0]} testId={'TestId'}>
-            Hello world
-        </AspectRatio>
-    );
-    expect(screen.getByTestId('TestId')).toBeTruthy();
-});
+    test('it works with specified testId', () => {
+        setup(
+            <AspectRatio ratio={ratios[0]} testId={'TestId'}>
+                Hello world
+            </AspectRatio>
+        );
+        expect(screen.getByTestId('TestId')).toBeTruthy();
+    });
 
-test('it works with as', () => {
-    setup(
-        <AspectRatio ratio={ratios[0]} as="span">
-            Hello world
-        </AspectRatio>
-    );
-    expect(screen.getByTestId('AspectRatio').nodeName).toBe('SPAN');
-});
+    test('it works with as', () => {
+        setup(
+            <AspectRatio ratio={ratios[0]} as="span">
+                Hello world
+            </AspectRatio>
+        );
+        expect(screen.getByTestId('AspectRatio').nodeName).toBe('SPAN');
+    });
 
-test('it works with className', () => {
-    setup(
-        <AspectRatio ratio={ratios[0]} className="test">
-            Hello world
-        </AspectRatio>
-    );
-    expect(
-        screen.getByTestId('AspectRatio').classList.contains('test')
-    ).toBeTruthy();
+    test('it works with className', () => {
+        setup(
+            <AspectRatio ratio={ratios[0]} className="test">
+                Hello world
+            </AspectRatio>
+        );
+        expect(
+            screen.getByTestId('AspectRatio').classList.contains('test')
+        ).toBeTruthy();
+    });
 });
--- src/components/Elevated/Elevated.spec.tsx
+++ src/components/Elevated/Elevated.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { expect, test, afterEach } from 'vitest';
+import { describe, expect, test, afterEach } from 'vitest';
 import { cleanup, render, screen } from '@testing-library/react';
 
 import Elevated, { ElevationHeight } from './index';
@@ -10,43 +10,45 @@ afterEach(cleanup);
 
 const elevations: ElevationHeight[] = [0, 1, 2, 3, 4];
 
-test.each(elevations)('it works with elevations', (elevation) => {
-    const { container } = setup(
-        <Elevated elevation={elevation}>Hello world</Elevated>
-    );
-    expect(container.firstChild).toMatchSnapshot();
-});
+describe('Elevated', () => {
+    test.each(elevations)('it works with elevations', (elevation) => {
+        const { container } = setup(
+            <Elevated elevation={elevation}>Hello world</Elevated>
+        );
+        expect(container.firstChild).toMatchSnapshot();
+    });
 
-test('it works with default testId', () => {
-    setup(<Elevated elevation={elevations[0]}>Hello world</Elevated>);
-    expect(screen.getByTestId('Elevated')).toBeTruthy();
-});
+    test('it works with default testId', () => {
+        setup(<Elevated elevation={elevations[0]}>Hello world</Elevated>);
+        expect(screen.getByTestId('Elevated')).toBeTruthy();
+    });
 
-test('it works with specified testId', () => {
-    setup(
-        <Elevated elevation={elevations[0]} testId={'TestId'}>
-            Hello world
-        </Elevated>
-    );
-    expect(screen.getByTestId('TestId')).toBeTruthy();
-});
+    test('it works with specified testId', () => {
+        setup(
+            <Elevated elevation={elevations[0]} testId={'TestId'}>
+                Hello world
+            </Elevated>
+        );
+        expect(screen.getByTestId('TestId')).toBeTruthy();
+    });
 
-test('it works with as', () => {
-    setup(
-        <Elevated elevation={elevations[0]} as="span">
-            Hello world
-        </Elevated>
-    );
-    expect(screen.getByTestId('Elevated').nodeName).toBe('SPAN');
-});
+    test('it works with as', () => {
+        setup(
+            <Elevated elevation={elevations[0]} as="span">
+                Hello world
+            </Elevated>
+        );
+        expect(screen.getByTestId('Elevated').nodeName).toBe('SPAN');
+    });
 
-test('it works with className', () => {
-    setup(
-        <Elevated elevation={elevations[0]} className="test">
-            Hello world
-        </Elevated>
-    );
-    expect(
-        screen.getByTestId('Elevated').classList.contains('test')
-    ).toBeTruthy();
+    test('it works with className', () => {
+        setup(
+            <Elevated elevation={elevations[0]} className="test">
+                Hello world
+            </Elevated>
+        );
+        expect(
+            screen.getByTestId('Elevated').classList.contains('test')
+        ).toBeTruthy();
+    });
 });
