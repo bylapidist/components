473b876ccb8f5af7e679e71d9e9c0bcfe4cf14f8
Brett Dorrans <hello@lapidist.net>
2023-03-15 21:39:19 +0000

    lint

--- src/patterns/Website.stories.tsx
+++ src/patterns/Website.stories.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import { BaseProps } from '../components/types';
 import ThemeProvider from '../components/ThemeProvider';
@@ -16,22 +16,38 @@ export default {
         options: {
             showToolbar: false,
             showPanel: false,
-            showAddonPanel: false,
+            showAddonPanel: false
         },
-        actions: { disable: true },
+        actions: { disable: true }
     }
 } as Meta;
 
 export const WebsiteCard: StoryFn<BaseProps> = () => (
     <Elevated elevation={2}>
-        <Box gutter={4} style={{ display: 'flex', flexDirection: 'column', gridGap: 'var(--size__2)' }}>
-            <Heading as="h2" size={3}>@lapidist/components</Heading>
+        <Box
+            gutter={4}
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gridGap: 'var(--size__2)'
+            }}
+        >
+            <Heading as="h2" size={3}>
+                @lapidist/components
+            </Heading>
             <Text variant="medium">
-                Nunc porttitor lectus ex, eu pharetra elit placerat non. Suspendisse nec
-                ultrices augue, et varius velit. Vestibulum ante ipsum primis in
-                faucibus orci luctus et ultrices posuere cubilia curae.
+                Nunc porttitor lectus ex, eu pharetra elit placerat non.
+                Suspendisse nec ultrices augue, et varius velit. Vestibulum ante
+                ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                curae.
             </Text>
-            <div style={{ display: 'flex', justifyContent: 'end', gridGap: 'var(--size__2)' }}>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'end',
+                    gridGap: 'var(--size__2)'
+                }}
+            >
                 <Link href="javascript:void()">github</Link>
                 <Link href="javascript:void()">npm</Link>
                 <Link href="javascript:void()">docs</Link>
