b4a26620ed20acd6996d674cb1a94a917c7a89dd
Brett Dorrans <hello@lapidist.net>
2023-03-26 16:11:57 +0100

    feat: upgrade @lapidist/linting

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,15 +1,2 @@
 const { stylelintConfig } = require('@lapidist/linting');
-module.exports = {
-    ...stylelintConfig,
-    // @TODO - lift to @lapidist/linting and fix these
-    extends: 'stylelint-config-standard-scss',
-    rules: {
-        'custom-property-pattern': null,
-        'keyframes-name-pattern': null,
-        'selector-class-pattern': null,
-        'block-no-empty': null
-    },
-    ignoreFiles: [
-        './node_modules/**'
-    ]
-};
+module.exports = stylelintConfig;
