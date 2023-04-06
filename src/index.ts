94a1b2538617777892f5aff9b0792decaae05238
Brett Dorrans <brett@bretts-imac.home>
2023-04-06 17:57:29 +0100

    tests

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export { default as Logo } from './components/Logo';
 export { default as Tag } from './components/Tag';
 export { default as Text } from './components/Text';
 export { default as ThemeProvider } from './components/ThemeProvider';
+export { default as Toggle } from './components/Toggle';
