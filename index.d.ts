0e9eed9f5c7e7272ecdd6df96f53f830cc560654
brettdorrans <hello@lapidist.net>
2023-07-01 11:47:07 +0000

    Deploying to gh-pages from  @ 021a10f36725307979ead54d969c2f201c4a3c8a 🚀

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+export { default as Box } from './components/Box';
+export { default as Button } from './components/Button';
+export { default as Heading } from './components/Heading';
+export { default as Link } from './components/Link';
+export { default as Logo } from './components/Logo';
+export { default as Tag } from './components/Tag';
+export { default as Text } from './components/Text';
+export { default as ThemeProvider } from './components/ThemeProvider';
+export { default as Toggle } from './components/Toggle';
