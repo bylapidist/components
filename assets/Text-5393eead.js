38eef3ec964aff8d7a0fed56fe300cd9c40bdaf6
brettdorrans <hello@lapidist.net>
2023-10-27 17:32:59 +0000

    Deploying to gh-pages from  @ 98729a5f3462923f5b30aa2d29c4ef8f5a10e197 🚀

---
+++ assets/Text-5393eead.js
@@ -0,0 +1,67 @@
+import{j as e}from"./index-48faf0e0.js";import{M as r,b as l,C as s,d as i,A as c}from"./index-d9229032.js";import{T as h,S as a,M as x,L as j,R as m,a as f,B as p,b as g,H as u,c as b,d as y}from"./Text.stories-ab72e109.js";import{u as d}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-40a94387.js";import"../sb-preview/runtime.js";import"./index-79dc52bf.js";import"./index-d37d4223.js";import"./index-356e4a49.js";import"./index-faf79eaa.js";function t(o){const n=Object.assign({h1:"h1",blockquote:"blockquote",p:"p",h2:"h2",code:"code",h3:"h3"},d(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{of:h}),`
+`,e.jsx(n.h1,{id:"text",children:"Text"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:`A primitive for controlling text. Use text to establish a consistent approach to constructing more
+complicated components, ensuring their typography is aligned with the theme. This can be used as a wrapping element
+for compound components and its prop API included to give the component text size, weight or family support.`}),`
+`]}),`
+`,e.jsx(n.h2,{id:"example",children:"Example"}),`
+`,e.jsx(l,{dark:!0}),`
+`,e.jsx(n.h2,{id:"controlling-font-size",children:"Controlling font size"}),`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"size"})," prop controls font size. There are three available font sizes, defined as ",e.jsx(n.code,{children:"small"}),", ",e.jsx(n.code,{children:"medium"})," and ",e.jsx(n.code,{children:"large"}),"."]}),`
+`,e.jsx(n.h3,{id:"small",children:"Small"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"small"})," size sets the font size to ",e.jsx(n.code,{children:"--font-size-200"})]}),`
+`]}),`
+`,e.jsx(s,{children:e.jsx(i,{of:a})}),`
+`,e.jsx(n.h3,{id:"medium",children:"Medium"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"medium"})," size sets the font size to ",e.jsx(n.code,{children:"--font-size-300"})]}),`
+`]}),`
+`,e.jsx(s,{children:e.jsx(i,{of:x})}),`
+`,e.jsx(n.h3,{id:"large",children:"Large"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"large"})," size sets the font size to ",e.jsx(n.code,{children:"--font-size-400"})]}),`
+`]}),`
+`,e.jsx(s,{children:e.jsx(i,{of:j})}),`
+`,e.jsx(n.h2,{id:"controlling-font-weight",children:"Controlling font weight"}),`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"weight"})," prop controls font weight. There are three available font weights, defined as ",e.jsx(n.code,{children:"regular"}),", ",e.jsx(n.code,{children:"medium"})," and ",e.jsx(n.code,{children:"bold"}),"."]}),`
+`,e.jsx(n.h3,{id:"regular",children:"Regular"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"regular"})," size sets the font weight to ",e.jsx(n.code,{children:"--font-weight-400"})]}),`
+`]}),`
+`,e.jsx(s,{children:e.jsx(i,{of:m})}),`
+`,e.jsx(n.h3,{id:"medium-1",children:"Medium"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"medium"})," size sets the font weight to ",e.jsx(n.code,{children:"--font-weight-500"})]}),`
+`]}),`
+`,e.jsx(s,{children:e.jsx(i,{of:f})}),`
+`,e.jsx(n.h3,{id:"bold",children:"Bold"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"bold"})," size sets the font weight to ",e.jsx(n.code,{children:"--font-weight-700"})]}),`
+`]}),`
+`,e.jsx(s,{children:e.jsx(i,{of:p})}),`
+`,e.jsx(n.h2,{id:"controlling-font-family",children:"Controlling font family"}),`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"variant"})," prop controls font family. There are three available font families, defined as ",e.jsx(n.code,{children:"sans"}),", ",e.jsx(n.code,{children:"serif"})," and ",e.jsx(n.code,{children:"mono"}),"."]}),`
+`,e.jsx(n.h3,{id:"sans",children:"Sans"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"body"})," variant sets the font family to ",e.jsx(n.code,{children:"--font-family-body"})]}),`
+`]}),`
+`,e.jsx(s,{children:e.jsx(i,{of:g})}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"heading"})," variant sets the font family to ",e.jsx(n.code,{children:"--font-family-heading"})]}),`
+`]}),`
+`,e.jsx(s,{children:e.jsx(i,{of:u})}),`
+`,e.jsx(n.h3,{id:"serif",children:"Serif"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"serif"})," variant sets the font family to ",e.jsx(n.code,{children:"--font-family-serif"})]}),`
+`]}),`
+`,e.jsx(s,{children:e.jsx(i,{of:b})}),`
+`,e.jsx(n.h3,{id:"mono",children:"Mono"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"mono"})," variant sets the font family to ",e.jsx(n.code,{children:"--font-family-mono"})]}),`
+`]}),`
+`,e.jsx(s,{children:e.jsx(i,{of:y})}),`
+`,e.jsx(n.h2,{id:"props",children:"Props"}),`
+`,e.jsx(c,{})]})}function L(o={}){const{wrapper:n}=Object.assign({},d(),o.components);return n?e.jsx(n,Object.assign({},o,{children:e.jsx(t,o)})):t(o)}export{L as default};
+//# sourceMappingURL=Text-5393eead.js.map
---
+++ assets/preview-452f4a38.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-40a94387.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-d8141a29.js"),["./DocsRenderer-NNNQARDV-d8141a29.js","./iframe-40a94387.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-e5767aa9.js","./index-79dc52bf.js","./index-d9229032.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-452f4a38.js.map
